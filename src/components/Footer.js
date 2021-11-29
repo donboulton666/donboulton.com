69d1513d6f3b98cf2ad381046067af9c93884c55
Donald W. Boulton <donaldboulton@gmail.com>
2021-11-29 10:07:47 -0600

    add fonts to security policy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Donald Boulton Personal Site. Design: <a href="/privacy">Basic Instructions</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js v4.2.0</a></p>
+        <p className="copyright">&copy; Donald Boulton Personal Site. Privacy: <a href="/privacy">Privacy</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js v4.2.0</a></p>
     </footer>
 )
 
