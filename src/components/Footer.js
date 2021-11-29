7e079d4a5657f79566f27fa16ad87569e023a525
Donald W. Boulton <donaldboulton@gmail.com>
2021-11-29 09:17:45 -0600

    privacy link image link to privacy policy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Donald Boulton Personal Site. Design: <a href="https://gatsbystarterbasicinstructions.gatsbyjs.io">Basic Instructions</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js v4.2.0</a></p>
+        <p className="copyright">&copy; Donald Boulton Personal Site. Design: <a href="/privacy">Basic Instructions</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js v4.2.0</a></p>
     </footer>
 )
 
